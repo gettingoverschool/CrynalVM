a32ef73291842bbf95e15bfd6af77fcffcbd7f9c
gettingoverschool <gettingoverschh@outlook.com>
2025-04-23 11:29:53 -0400

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/Blobby-Boi/BlobeVM
+git clone https://github.com/gettingoverschool/CrynalVM
 cd BlobeVM
 pip install textual
 sleep 2
@@ -14,9 +14,9 @@ cp -r BlobeVM/root/config/* Save
 
 json_file="BlobeVM/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=CrynalVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=CrynalVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
 else
-    docker run -d --name=BlobeVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=BlobeVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
+    docker run -d --name=CrynalVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=CrynalVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped blobevm
 fi
 clear
-echo "BLOBEVM WAS INSTALLED SUCCESSFULLY! Check Port Tab"
+echo "CRYNALVM WAS INSTALLED SUCCESSFULLY! Check Port Tab"
